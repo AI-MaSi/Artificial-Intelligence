8e4fede4d2af46a8473ce20af61552d84a747b33
Eetu <148777334+eetmie@users.noreply.github.com>
2024-02-12 14:36:43 +0200

    Update prepare OIGS.bat
    -hard coded isaac_sim -folder

--- OmniIsaacGymEnvs/prepare OIGS.bat
+++ OmniIsaacGymEnvs/prepare OIGS.bat
@@ -1,3 +1,3 @@
 @echo off
 start explorer "%USERPROFILE%\OmniIsaacGymEnvs\omniisaacgymenvs"
-cmd /k "cd %USERPROFILE%\OmniIsaacGymEnvs\omniisaacgymenvs && doskey PYTHON_PATH=%USERPROFILE%\AppData\Local\ov\pkg\isaac_sim-2023.*\python.bat $*"
+cmd /k "cd %USERPROFILE%\OmniIsaacGymEnvs\omniisaacgymenvs && doskey PYTHON_PATH=%USERPROFILE%\AppData\Local\ov\pkg\isaac_sim-2023.1.1\python.bat $*"
